031c62063dfb3be33f562f405e3a96b03fa2426d
Wanadorn <152370529+Wanadorn@users.noreply.github.com>
2024-02-28 14:28:24 +0700

    work8
    work8

--- register.php
+++ register.php
@@ -8,3 +8,81 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+    <link rel="stylesheet" href="bootstrap-5.3.2-dist/css/bootstrap.min.css">
+    <script src="bootstrap-5.3.2-dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <title>Register</title>
+</head>
+<body>
+    <div class="container-lg">
+        <h1 style="text-align: center;" class="mt-3">Webboard Wanadorn</h1>
+        <?php include "nav.php" ?>
+        <div class="row mt-4">
+            <div class="col-lg-4 col-md-3 col-sm-2 col-1"></div>
+            <div class="col-lg-4 col-md-6 col-sm-8 col-10">
+                <div class="card border-primary">
+                    <div class="card-header bg-primary text-white">เข้าสู่ระบบ</div>
+                    <div class="card-body">
+                        <form action="register_save.php" method="post">
+                            <div class="row">
+                                <label for="login" class="col-lg-3 col-form-label">ชื่อบัญชี:</label>
+                                <div class="col-lg-9">
+                                    <input id="login" type="text" name="login" class="form-control" require>
+                                </div>
+                            </div>
+                            <div class="row mt-3">
+                                <label for="pwd" class="col-lg-3 col-form-label">รหัสผ่าน:</label>
+                                <div class="col-lg-9">
+                                    <input id="pwd" type="password" name="pwd" class="form-control" require>
+                                </div>
+                            </div>
+                            <div class="row mt-3">
+                                <label for="name" class="col-lg-3 col-form-label">ชื่อ-นามสกุล:</label>
+                                <div class="col-lg-9">
+                                    <input id="name" type="text" name="name" class="form-control" require>
+                                </div>
+                            </div>
+                            <div class="row mt-3">
+                                <label class="col-lg-3 col-form-label">เพศ:</label>
+                                <div class="col-lg-9">
+                                    <div class="form-check">
+                                        <input id="m" type="radio" name="gender" value="m" class="form-check-input" require>
+                                        <label for="m" class="form-check-label">ชาย</label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input id="f" type="radio" name="gender" value="f" class="form-check-input" require>
+                                        <label for="f" class="form-check-label">หญิง</label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input id="o" type="radio" name="gender" value="o" class="form-check-input" require>
+                                        <label for="o" class="form-check-label">อื่นๆ</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row mt-3">
+                                <label for="email" class="col-lg-3 col-form-label">อีเมล:</label>
+                                <div>
+                                    <input id="email" type="email" name="email" class="form-control" require>
+                                </div>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-lg-12 d-flex justify-content-center">
+                                    <button type="submit" class="btn btn-primary btn-sm me-2">
+                                        <i class="bi bi-save"></i>สมัคสมาชิก
+                                    </button>
+                                    <button type="reset" class="btn btn-danger btn-sm">
+                                        <i class="bi bi-x-square"></i>ยกเลิก
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-3 col-sm-2 col-1"></div>
+        </div>
+    </div>    
+</body>
+</html>
